47a50f75875fbe6dabd6ed32197900858099e394
Krista <krista.abraham@gmail.com>
2018-04-02 18:26:51 -0400

    Basic client bootstrap setup

---
+++ client/src/ETH.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const ETH = (props) => {
+  return (
+    <div className="card">
+      <h5 className="card-header">ETH - USD</h5>
+      <div className="card-body">
+        <h5 className="card-title">Special title treatment</h5>
+        <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <a href="#" className="btn btn-primary">Do something</a>
+      </div>
+    </div>
+  );
+};
--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,12 +1,41 @@
 import React from 'react';
+import { BTC } from './BTC.jsx';
+import { ETH } from './ETH.jsx';
+import { BOTH } from './BTC-ETH.jsx';
+
+const style = {
+  gridContainer: {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(3, 1fr)',
+    gridTemplateRows: 'repeat(3, 1fr)',
+    gridGap: 10,
+    margin: 20,
+  },
+};
 
 export class App extends React.Component{
   constructor(props) {
     super(props);
+    this.state = {
+
+    };
   };
+
   render() {
     return (
-    <div>HELLO THERE</div>
+    <div style={style.gridContainer}>
+      <div></div>
+      <div></div>
+      <div></div>
+
+      <BTC {...this.props} />
+      <ETH {...this.props} />
+      <BOTH {...this.props} />
+
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
     );
   };
 };
