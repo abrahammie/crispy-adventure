166ab201c6175d60e1db053194e39915484153fa
Krista <krista.abraham@gmail.com>
2018-04-13 21:17:18 -0400

    Update order book layouts

--- client/src/BTC.jsx
+++ client/src/BTC.jsx
@@ -5,9 +5,14 @@ export const BTC = (props) => {
     <div className="card">
       <h5 className="card-header">BTC - USD</h5>
       <div className="card-body">
-        <h5 className="card-title">Special title treatment</h5>
-        <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a href="#" className="btn btn-primary">Do something</a>
+        <small className="card-text">Bid 3</small><br/>
+        <small className="card-text">Bid 2</small><br/>
+        <strong className="card-title">Best ask</strong>
+        <br/>
+        <br/>
+        <strong className="card-title">Best bid</strong><br/>
+        <small className="card-text">Bid 2</small><br/>
+        <small className="card-text">Bid 3</small>
       </div>
     </div>
   );
