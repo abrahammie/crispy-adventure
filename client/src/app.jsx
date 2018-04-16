fe9f49a3c03b6466db8bc34ffe906ca6ca15d8d2
Krista <krista.abraham@gmail.com>
2018-04-16 14:31:25 -0400

    Add socket.io to client to get BTC bids/asks

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import io from 'socket.io-client';
+var socket = io();
 import { BTC } from './BTC.jsx';
 import { ETH } from './ETH.jsx';
 import { BOTH } from './BTC-ETH.jsx';
+// import { Market } from './market.jsx';
 
 const style = {
   gridContainer: {
@@ -17,9 +20,22 @@ export class App extends React.Component{
   constructor(props) {
     super(props);
     this.state = {
-
+      BTCbids: [],
+      BTCasks: [],
     };
   };
+
+  componentWillMount() {
+    socket.on('topBTC', top => {
+      this.setState({ BTCbids: top.bids }, () => {
+        console.log('top bids:', this.state.BTCbids);
+      });
+      this.setState({ BTCasks: top.asks },  () => {
+        console.log('top asks:', this.state.BTCasks);
+      });
+    });
+  }
+
 
   render() {
     return (
@@ -27,6 +43,11 @@ export class App extends React.Component{
       <div></div>
       <div></div>
       <div></div>
+      {/*
+        this.state.markets.forEach((m) => {
+          <Market {...this.props} />
+        })
+      */}
 
       <BTC {...this.props} />
       <ETH {...this.props} />
