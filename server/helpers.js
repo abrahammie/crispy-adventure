6cedbdf1f6026d782597b91cf89dfb9036c67b1d
Krista <krista.abraham@gmail.com>
2018-05-10 11:22:59 -0400

    Refactor out binary search descending array

--- server/helpers.js
+++ server/helpers.js
@@ -1,11 +1,6 @@
 const addToAsks = (ordersArray, data) => {
   // console.log('adding to asks');
   let newArray = [...ordersArray];
-
-  // use binary search to find insertion index
-  // let high = newArray.length-1;
-  // let low = 0;
-  // let middle = Math.floor((high + low) / 2);
   let insertionSpot;
 
   // if order outside range, return
@@ -29,64 +24,35 @@ const addToAsks = (ordersArray, data) => {
       newArray.pop();
       return newArray;
     }
-    //   // if you've looked at all spots
-    //   } else if (middle === low) {
-    //     insertionSpot = high;
-    //     break;
-    //   } else if (Number.parseFloat(data[0]) > Number.parseFloat(newArray[middle][0])) {
-    //     low = middle;
-    //     middle = Math.floor((high + low) / 2);
-    //   } else if (Number.parseFloat(data[0]) < Number.parseFloat(newArray[middle][0])) {
-    //     high = middle;
-    //     middle = Math.floor((high + low) / 2);
-    //   }
-    // }
-
   }
 };
 
 const addToBids = (ordersArray, data) => {
   // console.log('adding to bids');
   let newArray = [...ordersArray];
-
-  // use binary search to find insertion index
-  let high = newArray.length-1;
-  let low = 0;
-  let middle = Math.floor((high + low) / 2);
   let insertionSpot;
 
   // if order outside range, return
-  if (Number.parseFloat(data[0]) < Number.parseFloat(newArray[high][0])) {
+  if (Number.parseFloat(data[0]) < Number.parseFloat(newArray[newArray.length-1][0])) {
     return newArray;
   // else if order better than top, add to front
-  } else if (Number.parseFloat(data[0]) > Number.parseFloat(newArray[low][0])) {
+  } else if (Number.parseFloat(data[0]) > Number.parseFloat(newArray[0][0])) {
     newArray.unshift(data);
     newArray.pop();
     return newArray;
   // else insert into descending sorted array
   } else {
-    while (high > low) {
-      // if price = newArray[middle]
-      if (Number.parseFloat(data[0]).toFixed(2) === Number.parseFloat(newArray[middle][0]).toFixed(2)) {
-        // increment order in place
-        let amt = Number.parseFloat(newArray[middle][1]) + Number.parseFloat(data[1]);
-        newArray[middle] = [newArray[middle][0], amt.toString(), ++newArray[middle][2]];
-        return newArray;
-      } else if (middle === low) {
-        insertionSpot = high;
-        break;
-      } else if (Number.parseFloat(data[0]) < Number.parseFloat(newArray[middle][0])) {
-        low = middle;
-        middle = Math.floor((high + low) / 2);
-      } else if (Number.parseFloat(data[0]) > Number.parseFloat(newArray[middle][0])) {
-        high = middle;
-        middle = Math.floor((high + low) / 2);
-      }
+    insertionSpot = binarySearchDescendingArray(newArray, data[0]);
+    if (Number.parseFloat(data[0]).toFixed(2) === Number.parseFloat(newArray[insertionSpot][0]).toFixed(2)) {
+      // increment order in place
+      let amt = Number.parseFloat(newArray[insertionSpot][1]) + Number.parseFloat(data[1]);
+      newArray[insertionSpot] = [newArray[insertionSpot][0], amt.toString(), ++newArray[insertionSpot][2]];
+      return newArray;
+    } else {
+      newArray.splice(insertionSpot, 0, data);
+      newArray.pop();
+      return newArray;
     }
-
-    newArray.splice(insertionSpot, 0, data);
-    newArray.pop();
-    return newArray;
   }
 };
 
@@ -160,6 +126,30 @@ const binarySearchAscendingArray = (array, targetPrice, high, low) => {
       low = middle;
       middle = Math.floor((high + low) / 2);
     } else if (Number.parseFloat(targetPrice) < Number.parseFloat(array[middle][0])) {
+      high = middle;
+      middle = Math.floor((high + low) / 2);
+    }
+  }
+};
+
+// returns insertion or match index
+const binarySearchDescendingArray = (array, targetPrice, high, low) => {
+  high = high || array.length-1;
+  low = low || 0;
+  let middle = Math.floor((high + low) / 2);
+
+  while (high > low) {
+    // if price matches the order being evaluated
+    if (Number.parseFloat(targetPrice).toFixed(2) === Number.parseFloat(array[middle][0]).toFixed(2)) {
+      return middle;
+    // if you've looked at all spots
+    } else if (middle === low) {
+      return high;
+    // else keep searching
+    } else if (Number.parseFloat(targetPrice) < Number.parseFloat(array[middle][0])) {
+      low = middle;
+      middle = Math.floor((high + low) / 2);
+    } else if (Number.parseFloat(targetPrice) > Number.parseFloat(array[middle][0])) {
       high = middle;
       middle = Math.floor((high + low) / 2);
     }
